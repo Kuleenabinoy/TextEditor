4ac4435f3f40bfe4a346b2f98cf27f6c5ad9a182
leena <annachandy123@gmail.com>
2022-01-10 10:54:32 +1100

    error fixed

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -12,11 +12,10 @@ const initdb = async () =>
         },
     });
 
-// TODO: Add logic to a method that accepts some content and adds it to the database
-//export const putDb = async (content) => console.error("putDb not implemented");
+// TODO: Add logic to a method that accepts some content and adds it to the database---DONE
 
-// // TODO: Add logic for a method that gets all the content from the database
-//export const getDb = async () => console.error("getDb not implemented");
+// // TODO: Add logic for a method that gets all the content from the database---DONE
+
 // Export a function we will use to GET to the database.
 export const putDb = async (content) => {
     console.log("PUT to the database");
